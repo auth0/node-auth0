5fb54223085a23eb66c7f7ef9617f07498538a06
Nicolás Gonzálvez <cristopher.gonzalvez@gmail.con>
2015-10-30 10:21:31 -0300

    Add constants.js file

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+var exports = module.exports = {};
+
+/**
+ * Object containing the Auth0 domain for each region server.
+ */
+exports.DOMAINS_BY_REGION = {
+  us: 'login.auth0.com',
+  eu: 'login.eu.auth0.com'
+};
+
+/**
+ * Default Auth0 region servers.
+ */
+exports.DEFAULT_REGION = 'us';
