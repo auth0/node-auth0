04e187ca8f7ad68bdf0c87bb1f38a2c4e6a3b385
Nicolás Gonzálvez <cristopher.gonzalvez@gmail.con>
2015-11-13 14:25:39 -0300

    Improve Auth0 module tests

--- test/auth0.tests.js
+++ test/auth0.tests.js
@@ -7,18 +7,22 @@ var domains = require('../src/constants').DOMAINS_BY_REGION;
 
 describe('Auth0 module', function () {
 
+    it('should raise an error when no options object is provided', function () {
+      expect(auth0).to.throw(ArgumentError, 'Auth0 SDK options must be an object');
+    });
+
+    it('should raise an error when the token is not valid', function () {
+      var client = auth0.bind(null, { token: '' });
+
+      expect(client).to.throw(ArgumentError, 'An access token must be provided');
+    });
+
     it('should return an instance of Auth0 when the token is valid', function () {
       var options = { token: 'abcdefg' };
-      var client = null;
+      var client = auth0(options);
 
-      try {
-        client = auth0(options);
-
-        expect(client).to.be.an('Object');
-        expect(client).to.be.an.instanceOf(Auth0);
-      } catch (err) {
-        done(err);
-      }
+      expect(client).to.be.an('Object');
+      expect(client).to.be.an.instanceOf(Auth0);
     });
 
     it('should not allow providing both domain and region', function () {
@@ -32,7 +36,7 @@ describe('Auth0 module', function () {
       expect(client).to.throw(ArgumentError, 'Cannot provide both region and domain');
     });
 
-    it('should error when a wrong region is provided', function () {
+    it('should raise an error when a wrong region is provided', function () {
       var options = {
         token: 'token',
         region: 'xy'
@@ -42,7 +46,7 @@ describe('Auth0 module', function () {
       expect(client).to.throw(ArgumentError);
     });
 
-    it('should not error when a valid region is provided', function () {
+    it('should not throw errors when a valid region is provided', function () {
       var options = {
         token: 'token',
         region: 'eu'
