3276fd26db9b530b5f8f0ba1e56ad68a9bba2b3d
Nicolás Gonzálvez <cristopher.gonzalvez@gmail.com>
2015-12-03 15:06:12 -0300

    Fix the tests for the AuthenticationClient class

--- test/auth/authentication-client.tests.js
+++ test/auth/authentication-client.tests.js
@@ -9,21 +9,15 @@ var PasswordlessAuthenticator = require('../../src/auth/PasswordlessAuthenticato
 var UsersManager = require('../../src/auth/UsersManager');
 var TokensManager = require('../../src/auth/TokensManager');
 
+var ensureProperty = require('../utils').ensureProperty;
+
 
 describe('AuthenticationClient', function () {
 
+  describe('#constructor', function () {
     it('should raise an error when no options object is provided', function () {
       expect(AuthenticationClient)
         .to.throw(ArgumentError, 'Authentication Client SDK options must be an object');
-    });
-
-
-    it('should raise an error when the token is not valid', function () {
-      var options = { token: '', domain: 'tenant.auth.com' };
-      var client = AuthenticationClient.bind(null, options);
-
-      expect(client)
-        .to.throw(ArgumentError, 'An access token must be provided');
     });
 
 
@@ -33,67 +27,54 @@ describe('AuthenticationClient', function () {
       expect(client)
         .to.throw(ArgumentError, 'Must provide a domain');
     });
+  });
 
 
-    describe('instance properties', function () {
-      var property;
-      var properties = {
-        'OAuthAuthenticator': {
-          property: 'oauth',
-          cls: OAuthAuthenticator
-        },
-        'DatabaseAuthenticator': {
-          property: 'database',
-          cls: DatabaseAuthenticator
-        },
-        'PasswordlessAuthenticator': {
-          property: 'passwordless',
-          cls: PasswordlessAuthenticator
-        },
-        'UsersManager': {
-          property: 'users',
-          cls: UsersManager
-        },
-        'TokensManager': {
-          property: 'tokens',
-          cls: TokensManager
-        }
-      };
-
-      before(function () {
-        var options = { token: 'token', domain: 'tenant.auth0.com' };
-        this.client = new AuthenticationClient(options);
-
-        // Tests common to all properties.
-        for (var name in properties) {
-          property = properties[name];
-
-          it('should expose an instance of ' + name, function () {
-            expect(this.client[property.property])
-              .to.exist
-              .to.be.an.instanceOf(property.cls);
-          });
-        }
-      });
-    });
-
-
-    describe('instance methods', function () {
-      var method;
-      var methods = [];
-
-      before(function () {
-        this.client = new AuthenticationClient({ token: 'token', domain: 'auth0.com' });
-      });
-
-      for (var i = 0, l = methods.length; i < l; i++) {
-        method = methods[i];
-
-        it('should have a ' + method + ' method', function () {
-          expect(this.client[method])
-            .to.exist
-            .to.be.an.instanceOf(Function);
-        });
+  describe('instance properties', function () {
+    var properties = {
+      'OAuthAuthenticator': {
+        name: 'oauth',
+        cls: OAuthAuthenticator
+      },
+      'DatabaseAuthenticator': {
+        name: 'database',
+        cls: DatabaseAuthenticator
+      },
+      'PasswordlessAuthenticator': {
+        name: 'passwordless',
+        cls: PasswordlessAuthenticator
+      },
+      'UsersManager': {
+        name: 'users',
+        cls: UsersManager
+      },
+      'TokensManager': {
+        name: 'tokens',
+        cls: TokensManager
       }
+    };
+    var options = {
+      clientId: 'CLIENT_ID',
+      domain: 'tenant.auth0.com'
+    };
+    var client = new AuthenticationClient(options);
+
+    // Tests common to all properties.
+    for (var name in properties) {
+      var property = properties[name];
+
+      it('should expose an instance of ' + name, ensureProperty(client, property.name, property.cls));
+    }
+  });
+
+
+  describe('instance methods', function () {
+    var method;
+    var methods = [];
+    var client = new AuthenticationClient({ token: 'token', domain: 'auth0.com' });
+
+    methods.forEach(function (method) {
+      ensureMethod(client, method);
     });
+  });
 });
