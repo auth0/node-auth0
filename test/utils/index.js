902fa1a336ca5c1e1f095d87be8df2c327cf5cca
Nicolás Gonzálvez <cristopher.gonzalvez@gmail.com>
2015-12-03 15:04:43 -0300

    Add utility functions

--- test/utils/index.js
+++ test/utils/index.js
@@ -1,3 +1,5 @@
 module.exports = {
-  extractParts: require('./extractParts')
+  extractParts: require('./extractParts'),
+  ensureProperty: require('./ensureProperty'),
+  ensureMethod: require('./ensureMethod')
 };
