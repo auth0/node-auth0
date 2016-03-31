83ae3a5c9dc59271fa40bbfc0465dd608a1763f4
manu <emmanuel.bourmalo@gmail.com>
2016-03-31 13:32:23 +0200

    fix the content-type header check

--- src/utils.js
+++ src/utils.js
@@ -73,7 +73,8 @@ utils.getRequestPromise = function (settings) {
         return;
       }
 
-      if (res.header['content-type'].substr(0, 5) === 'text/') {
+      if (res.header['content-type']
+      && res.header['content-type'].substr(0, 5) === 'text/') {
         return resolve(res.text);
       }
 
