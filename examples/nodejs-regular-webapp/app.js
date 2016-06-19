aceb4162123275bdef17844b44ecf1b0eee00b4e
Vlad <pleo12@gmail.com>
2016-06-19 16:01:40 +0300

    dependencies update (also 'jade' is now 'pug')

--- examples/nodejs-regular-webapp/app.js
+++ examples/nodejs-regular-webapp/app.js
@@ -42,7 +42,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
