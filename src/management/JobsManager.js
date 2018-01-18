9a1cb7578490abebc56e9b2a776f91381b17e50d
Chaz <chaz@plaidpotion.com>
2018-01-18 11:32:33 -0500

    arg must be string

--- src/management/JobsManager.js
+++ src/management/JobsManager.js
@@ -129,7 +129,7 @@ JobsManager.prototype.importUsers = function (data, cb) {
     var req = request
       .post(options.baseUrl + '/jobs/users-imports')
         .field('connection_id', data.connection_id)
-        .field('send_completion_email', !!data.send_completion_email)
+        .field('send_completion_email', (!!data.send_completion_email).toString())
         .attach('users', data.users);
 
     for (var name in headers) {
