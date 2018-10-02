8550e91d20599810198db24a0543f883693a90d2
Vikas <potlurivikas@gmail.com>
2018-10-02 09:47:47 -0500

    chore(es6): Migrate src/management/ConnectionsManager.js

--- src/management/ConnectionsManager.js
+++ src/management/ConnectionsManager.js
@@ -1,7 +1,7 @@
-var ArgumentError = require('rest-facade').ArgumentError;
-var utils = require('../utils');
-var Auth0RestClient = require('../Auth0RestClient');
-var RetryRestClient = require('../RetryRestClient');
+const { ArgumentError } = require('rest-facade');
+const { wrapPropertyMethod } = require('../utils');
+const Auth0RestClient = require('../Auth0RestClient');
+const RetryRestClient = require('../RetryRestClient');
 
 /**
  * @class ConnectionsManager
@@ -14,43 +14,47 @@ var RetryRestClient = require('../RetryRestClient');
  * @param {Object} [options.headers]  Headers to be included in all requests.
  * @param {Object} [options.retry]    Retry Policy Config
  */
-var ConnectionsManager = function(options) {
-  if (options === null || typeof options !== 'object') {
-    throw new ArgumentError('Must provide client options');
+class ConnectionsManager {
+  constructor(options) {
+    if (options === null || typeof options !== 'object') {
+      throw new ArgumentError('Must provide client options');
+    }
+
+    if (options.baseUrl === null || options.baseUrl === undefined) {
+      throw new ArgumentError('Must provide a base URL for the API');
+    }
+
+    if ('string' !== typeof options.baseUrl || options.baseUrl.length === 0) {
+      throw new ArgumentError('The provided base URL is invalid');
+    }
+
+    const { headers, baseUrl, tokenProvider, retry } = options;
+
+    /**
+     * Options object for the Rest Client instance.
+     *
+     * @type {Object}
+     */
+    const clientOptions = {
+      headers,
+      query: { repeatParams: false }
+    };
+
+    /**
+     * Provides an abstraction layer for performing CRUD operations on
+     * {@link https://auth0.com/docs/api/v2#!/ConnectionsManagers Auth0
+     *  Connections}.
+     *
+     * @type {external:RestClient}
+     */
+    const auth0RestClient = new Auth0RestClient(
+      `${baseUrl}/connections/:id`,
+      clientOptions,
+      tokenProvider
+    );
+    this.resource = new RetryRestClient(auth0RestClient, retry);
   }
-
-  if (options.baseUrl === null || options.baseUrl === undefined) {
-    throw new ArgumentError('Must provide a base URL for the API');
-  }
-
-  if ('string' !== typeof options.baseUrl || options.baseUrl.length === 0) {
-    throw new ArgumentError('The provided base URL is invalid');
-  }
-
-  /**
-   * Options object for the Rest Client instance.
-   *
-   * @type {Object}
-   */
-  var clientOptions = {
-    headers: options.headers,
-    query: { repeatParams: false }
-  };
-
-  /**
-   * Provides an abstraction layer for performing CRUD operations on
-   * {@link https://auth0.com/docs/api/v2#!/ConnectionsManagers Auth0
-   *  Connections}.
-   *
-   * @type {external:RestClient}
-   */
-  var auth0RestClient = new Auth0RestClient(
-    options.baseUrl + '/connections/:id ',
-    clientOptions,
-    options.tokenProvider
-  );
-  this.resource = new RetryRestClient(auth0RestClient, options.retry);
-};
+}
 
 /**
  * Create a new connection.
@@ -64,7 +68,7 @@ var ConnectionsManager = function(options) {
  *     // Handle error.
  *   }
  *
- *   // Conection created.
+ *   // Connection created.
  * });
  *
  * @param   {Object}    data     Connection data object.
@@ -72,7 +76,7 @@ var ConnectionsManager = function(options) {
  *
  * @return  {Promise|undefined}
  */
-utils.wrapPropertyMethod(ConnectionsManager, 'create', 'resource.create');
+wrapPropertyMethod(ConnectionsManager, 'create', 'resource.create');
 
 /**
  * Get all connections.
@@ -87,7 +91,7 @@ utils.wrapPropertyMethod(ConnectionsManager, 'create', 'resource.create');
  * </caption>
  *
  * // Pagination settings.
- * var params = {
+ * const params = {
  *   per_page: 10,
  *   page: 0
  * };
@@ -103,7 +107,7 @@ utils.wrapPropertyMethod(ConnectionsManager, 'create', 'resource.create');
  *
  * @return  {Promise|undefined}
  */
-utils.wrapPropertyMethod(ConnectionsManager, 'getAll', 'resource.getAll');
+wrapPropertyMethod(ConnectionsManager, 'getAll', 'resource.getAll');
 
 /**
  * Get an Auth0 connection.
@@ -126,7 +130,7 @@ utils.wrapPropertyMethod(ConnectionsManager, 'getAll', 'resource.getAll');
  *
  * @return  {Promise|undefined}
  */
-utils.wrapPropertyMethod(ConnectionsManager, 'get', 'resource.get');
+wrapPropertyMethod(ConnectionsManager, 'get', 'resource.get');
 
 /**
  * Update an existing connection.
@@ -135,8 +139,8 @@ utils.wrapPropertyMethod(ConnectionsManager, 'get', 'resource.get');
  * @memberOf  module:management.ConnectionsManager.prototype
  *
  * @example
- * var data = { name: 'newConnectionName' };
- * var params = { id: CONNECTION_ID };
+ * const data = { name: 'newConnectionName' };
+ * const params = { id: CONNECTION_ID };
  *
  * management.connections.update(params, data, function (err, connection) {
  *   if (err) {
@@ -146,14 +150,14 @@ utils.wrapPropertyMethod(ConnectionsManager, 'get', 'resource.get');
  *   console.log(connection.name);  // 'newConnectionName'
  * });
  *
- * @param   {Object}    params        Conneciton parameters.
+ * @param   {Object}    params        Connection parameters.
  * @param   {String}    params.id     Connection ID.
  * @param   {Object}    data          Updated connection data.
  * @param   {Function}  [cb]          Callback function.
  *
  * @return    {Promise|undefined}
  */
-utils.wrapPropertyMethod(ConnectionsManager, 'update', 'resource.patch');
+wrapPropertyMethod(ConnectionsManager, 'update', 'resource.patch');
 
 /**
  * Delete an existing connection.
@@ -167,7 +171,7 @@ utils.wrapPropertyMethod(ConnectionsManager, 'update', 'resource.patch');
  *     // Handle error.
  *   }
  *
- *   // Conection deleted.
+ *   // Connection deleted.
  * });
  *
  * @param   {Object}    params          Connection parameters.
@@ -176,6 +180,6 @@ utils.wrapPropertyMethod(ConnectionsManager, 'update', 'resource.patch');
  *
  * @return  {Promise|undefined}
  */
-utils.wrapPropertyMethod(ConnectionsManager, 'delete', 'resource.delete');
+wrapPropertyMethod(ConnectionsManager, 'delete', 'resource.delete');
 
 module.exports = ConnectionsManager;
