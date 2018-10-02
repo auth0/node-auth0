81c22e948db7efc3f25fe9b8b085b3f65f3fd3e1
Vikas <potlurivikas@gmail.com>
2018-10-02 09:34:15 -0500

    chore(es6): Migrate src/management/BlacklistedTokensManager.js

--- src/management/BlacklistedTokensManager.js
+++ src/management/BlacklistedTokensManager.js
@@ -1,7 +1,7 @@
-var ArgumentError = require('rest-facade').ArgumentError;
-var utils = require('../utils');
-var Auth0RestClient = require('../Auth0RestClient');
-var RetryRestClient = require('../RetryRestClient');
+const { ArgumentError } = require('rest-facade');
+const { wrapPropertyMethod } = require('../utils');
+const Auth0RestClient = require('../Auth0RestClient');
+const RetryRestClient = require('../RetryRestClient');
 
 /**
  * @class BlacklistedTokensManager
@@ -15,43 +15,47 @@ var RetryRestClient = require('../RetryRestClient');
  * @param {Object} [options.headers]  Headers to be included in all requests.
  * @param {Object} [options.retry]    Retry Policy Config
  */
-var BlacklistedTokensManager = function(options) {
-  if (options === null || typeof options !== 'object') {
-    throw new ArgumentError('Must provide client options');
+class BlacklistedTokensManager {
+  constructor(options) {
+    if (options === null || typeof options !== 'object') {
+      throw new ArgumentError('Must provide client options');
+    }
+
+    if (options.baseUrl === null || options.baseUrl === undefined) {
+      throw new ArgumentError('Must provide a base URL for the API');
+    }
+
+    if ('string' !== typeof options.baseUrl || options.baseUrl.length === 0) {
+      throw new ArgumentError('The provided base URL is invalid');
+    }
+
+    const { headers, retry, baseUrl, tokenProvider } = options;
+
+    /**
+     * Options object for the Rest Client instace.
+     *
+     * @type {Object}
+     */
+    const clientOptions = {
+      errorFormatter: { message: 'message', name: 'error' },
+      headers,
+      query: { repeatParams: false }
+    };
+
+    /**
+     * Provides an abstraction layer for consuming the
+     * [Auth0 Blacklisted Tokens endpoint]{@link https://auth0.com/docs/api/v2#!/Clients}.
+     *
+     * @type {external:RestClient}
+     */
+    const auth0RestClient = new Auth0RestClient(
+      `${baseUrl}/blacklists/tokens`,
+      clientOptions,
+      tokenProvider
+    );
+    this.resource = new RetryRestClient(auth0RestClient, retry);
   }
-
-  if (options.baseUrl === null || options.baseUrl === undefined) {
-    throw new ArgumentError('Must provide a base URL for the API');
-  }
-
-  if ('string' !== typeof options.baseUrl || options.baseUrl.length === 0) {
-    throw new ArgumentError('The provided base URL is invalid');
-  }
-
-  /**
-   * Options object for the Rest Client instace.
-   *
-   * @type {Object}
-   */
-  var clientOptions = {
-    errorFormatter: { message: 'message', name: 'error' },
-    headers: options.headers,
-    query: { repeatParams: false }
-  };
-
-  /**
-   * Provides an abstraction layer for consuming the
-   * [Auth0 Blacklisted Tokens endpoint]{@link https://auth0.com/docs/api/v2#!/Clients}.
-   *
-   * @type {external:RestClient}
-   */
-  var auth0RestClient = new Auth0RestClient(
-    options.baseUrl + '/blacklists/tokens',
-    clientOptions,
-    options.tokenProvider
-  );
-  this.resource = new RetryRestClient(auth0RestClient, options.retry);
-};
+}
 
 /**
  * Blacklist a new token.
@@ -60,7 +64,7 @@ var BlacklistedTokensManager = function(options) {
  * @memberOf  module:management.BlacklistedTokensManager.prototype
  *
  * @example
- * var token = {
+ * const token = {
  *  aud: 'aud',
  *  jti: 'jti'
  * };
@@ -80,7 +84,7 @@ var BlacklistedTokensManager = function(options) {
  *
  * @return  {Promise|undefined}
  */
-utils.wrapPropertyMethod(BlacklistedTokensManager, 'add', 'resource.create');
+wrapPropertyMethod(BlacklistedTokensManager, 'add', 'resource.create');
 
 /**
  * Get all blacklisted tokens.
@@ -97,6 +101,6 @@ utils.wrapPropertyMethod(BlacklistedTokensManager, 'add', 'resource.create');
  *
  * @return  {Promise|undefined}
  */
-utils.wrapPropertyMethod(BlacklistedTokensManager, 'getAll', 'resource.getAll');
+wrapPropertyMethod(BlacklistedTokensManager, 'getAll', 'resource.getAll');
 
 module.exports = BlacklistedTokensManager;
