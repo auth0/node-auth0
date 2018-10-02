b98093d2a368180375b04f099188541ec8eb81cb
Vikas <potlurivikas@gmail.com>
2018-10-02 10:45:27 -0500

    chore(es6): Migrate src/management/StatsManager.js

--- src/management/StatsManager.js
+++ src/management/StatsManager.js
@@ -1,6 +1,6 @@
-var ArgumentError = require('rest-facade').ArgumentError;
-var Auth0RestClient = require('../Auth0RestClient');
-var RetryRestClient = require('../RetryRestClient');
+const { ArgumentError } = require('rest-facade');
+const Auth0RestClient = require('../Auth0RestClient');
+const RetryRestClient = require('../RetryRestClient');
 
 /**
  * Simple facade for consuming a REST API endpoint.
@@ -19,105 +19,108 @@ var RetryRestClient = require('../RetryRestClient');
  * @param {Object} [options.headers]  Headers to be included in all requests.
  * @param {Object} [options.retry]    Retry Policy Config
  */
-var StatsManager = function(options) {
-  if (options === null || typeof options !== 'object') {
-    throw new ArgumentError('Must provide manager options');
-  }
+class StatsManager {
+  constructor(options) {
+    if (options === null || typeof options !== 'object') {
+      throw new ArgumentError('Must provide manager options');
+    }
 
-  if (options.baseUrl === null || options.baseUrl === undefined) {
-    throw new ArgumentError('Must provide a base URL for the API');
-  }
+    if (options.baseUrl === null || options.baseUrl === undefined) {
+      throw new ArgumentError('Must provide a base URL for the API');
+    }
 
-  if ('string' !== typeof options.baseUrl || options.baseUrl.length === 0) {
-    throw new ArgumentError('The provided base URL is invalid');
-  }
+    if ('string' !== typeof options.baseUrl || options.baseUrl.length === 0) {
+      throw new ArgumentError('The provided base URL is invalid');
+    }
 
-  var clientOptions = {
-    errorFormatter: { message: 'message', name: 'error' },
-    headers: options.headers,
-    query: { repeatParams: false }
-  };
+    const { headers, baseUrl, tokenProvider, retry } = options;
+
+    const clientOptions = {
+      errorFormatter: { message: 'message', name: 'error' },
+      headers,
+      query: { repeatParams: false }
+    };
+
+    /**
+     * Provides an abstraction layer for consuming the
+     * {@link https://auth0.com/docs/api/v2#!/Stats Stats endpoint}.
+     *
+     * @type {external:RestClient}
+     */
+    const auth0RestClient = new Auth0RestClient(
+      `${baseUrl}/stats/:type`,
+      clientOptions,
+      tokenProvider
+    );
+    this.stats = new RetryRestClient(auth0RestClient, retry);
+  }
 
   /**
-   * Provides an abstraction layer for consuming the
-   * {@link https://auth0.com/docs/api/v2#!/Stats Stats endpoint}.
+   * Get the daily stats.
    *
-   * @type {external:RestClient}
+   * @method    getDaily
+   * @memberOf  module:management.StatsManager.prototype
+   *
+   * @example
+   * const params = {
+   *   from: '{YYYYMMDD}',  // First day included in the stats.
+   *   to: '{YYYYMMDD}'  // Last day included in the stats.
+   * };
+   *
+   * management.stats.getDaily(params, function (err, stats) {
+   *   if (err) {
+   *     // Handle error.
+   *   }
+   *
+   *   console.log(stats);
+   * });
+   *
+   * @param   {Object}    params        Stats parameters.
+   * @param   {String}    params.from   The first day in YYYYMMDD format.
+   * @param   {String}    params.to     The last day in YYYYMMDD format.
+   * @param   {Function}  [cb]          Callback function.
+   *
+   * @return  {Promise|undefined}
    */
-  var auth0RestClient = new Auth0RestClient(
-    options.baseUrl + '/stats/:type',
-    clientOptions,
-    options.tokenProvider
-  );
-  this.stats = new RetryRestClient(auth0RestClient, options.retry);
-};
+  getDaily(params = {}, cb) {
+    params.type = 'daily';
 
-/**
- * Get the daily stats.
- *
- * @method    getDaily
- * @memberOf  module:management.StatsManager.prototype
- *
- * @example
- * var params = {
- *   from: '{YYYYMMDD}',  // First day included in the stats.
- *   to: '{YYYYMMDD}'  // Last day included in the stats.
- * };
- *
- * management.stats.getDaily(params, function (err, stats) {
- *   if (err) {
- *     // Handle error.
- *   }
- *
- *   console.log(stats);
- * });
- *
- * @param   {Object}    params        Stats parameters.
- * @param   {String}    params.from   The first day in YYYYMMDD format.
- * @param   {String}    params.to     The last day in YYYYMMDD format.
- * @param   {Function}  [cb]          Callback function.
- *
- * @return  {Promise|undefined}
- */
-StatsManager.prototype.getDaily = function(params, cb) {
-  params = params || {};
-  params.type = 'daily';
+    if (cb && cb instanceof Function) {
+      return this.stats.get(params, cb);
+    }
 
-  if (cb && cb instanceof Function) {
-    return this.stats.get(params, cb);
+    return this.stats.get(params);
   }
 
-  return this.stats.get(params);
-};
+  /**
+   * Get a the active users count.
+   *
+   * @method    getActiveUsersCount
+   * @memberOf  module:management.StatsManager.prototype
+   *
+   * @example
+   * management.stats.getActiveUsersCount(function (err, usersCount) {
+   *   if (err) {
+   *     // Handle error.
+   *   }
+   *
+   *   console.log(usersCount);
+   * });
+   *
+   * @param   {Function}  [cb]  Callback function.
+   *
+   * @return  {Promise|undefined}
+   */
+  getActiveUsersCount(cb) {
+    const options = { type: 'active-users' };
 
-/**
- * Get a the active users count.
- *
- * @method    getActiveUsersCount
- * @memberOf  module:management.StatsManager.prototype
- *
- * @example
- * management.stats.getActiveUsersCount(function (err, usersCount) {
- *   if (err) {
- *     // Handle error.
- *   }
- *
- *   console.log(usersCount);
- * });
- *
- * @param   {Function}  [cb]  Callback function.
- *
- * @return  {Promise|undefined}
- */
-StatsManager.prototype.getActiveUsersCount = function(cb) {
-  var options = { type: 'active-users' };
+    if (cb && cb instanceof Function) {
+      return this.stats.get(options, cb);
+    }
 
-  if (cb && cb instanceof Function) {
-    return this.stats.get(options, cb);
+    // Return a promise.
+    return this.stats.get(options);
   }
-
-  // Return a promise.
-  return this.stats.get(options);
-};
+}
 
 module.exports = StatsManager;
