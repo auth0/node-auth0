7b0e38e539f5eb8067e04a96b37c92571e8d191e
Vikas <potlurivikas@gmail.com>
2018-10-02 08:26:25 -0500

    chore(es6): Migrate src/auth/OAuthAuthenticator.js

--- src/auth/OAuthAuthenticator.js
+++ src/auth/OAuthAuthenticator.js
@@ -1,9 +1,5 @@
-var extend = require('util')._extend;
-
-var ArgumentError = require('rest-facade').ArgumentError;
-var RestClient = require('rest-facade').Client;
-
-var OAUthWithIDTokenValidation = require('./OAUthWithIDTokenValidation');
+const { ArgumentError, Client: RestClient } = require('rest-facade');
+const OAUthWithIDTokenValidation = require('./OAUthWithIDTokenValidation');
 
 /**
  * @class
@@ -18,302 +14,304 @@ var OAUthWithIDTokenValidation = require('./OAUthWithIDTokenValidation');
  * @param  {String}              [options.clientId]     Default client ID.
  * @param  {String}              [options.clientSecret] Default client Secret.
  */
-var OAuthAuthenticator = function(options) {
-  if (!options) {
-    throw new ArgumentError('Missing authenticator options');
-  }
+class OAuthAuthenticator {
+  constructor(options) {
+    if (!options) {
+      throw new ArgumentError('Missing authenticator options');
+    }
 
-  if (typeof options !== 'object') {
-    throw new ArgumentError('The authenticator options must be an object');
+    if (typeof options !== 'object') {
+      throw new ArgumentError('The authenticator options must be an object');
+    }
+
+    /**
+     * Options object for the Rest Client instance.
+     *
+     * @type {Object}
+     */
+    const clientOptions = {
+      errorFormatter: { message: 'message', name: 'error' }
+    };
+
+    const { clientId, clientSecret, baseUrl } = options;
+
+    this.oauth = new RestClient(`${baseUrl}/oauth/:type`, clientOptions);
+    this.oauthWithIDTokenValidation = new OAUthWithIDTokenValidation(this.oauth, options);
+    this.clientId = clientId;
+    this.clientSecret = clientSecret;
   }
 
   /**
-   * Options object for the Rest Client instace.
+   * Sign in using a username and password.
    *
-   * @type {Object}
+   * @method    signIn
+   * @memberOf  module:auth.OAuthAuthenticator.prototype
+   *
+   * @example <caption>
+   *   Given the user's credentials and the connection specified, it
+   *   will return a JSON with the access_token and id_token.
+   *   More information in the
+   *   <a href="https://auth0.com/docs/api/authentication#database-ad-ldap-active-">
+   *     API Docs
+   *   </a>.
+   * </caption>
+   *
+   * const data = {
+   *   client_id: '{CLIENT_ID}',  // Optional field.
+   *   username: '{USERNAME}',
+   *   password: '{PASSWORD}',
+   *   connection: '{CONNECTION_NAME}',
+   *   scope: 'openid'  // Optional field.
+   * };
+   *
+   * auth0.oauth.signIn(data, function (err, userData) {
+   *   if (err) {
+   *     // Handle error.
+   *   }
+   *
+   *   console.log(userData);
+   * });
+   *
+   * @param   {Object}    userData              User credentials object.
+   * @param   {String}    userData.username     Username.
+   * @param   {String}    userData.password     User password.
+   * @param   {String}    userData.connection   The identity provider in use.
+   *
+   * @return  {Promise|undefined}
    */
-  var clientOptions = {
-    errorFormatter: { message: 'message', name: 'error' }
-  };
+  signIn(userData, cb) {
+    const params = { type: 'ro' };
+    const data = Object.assign(
+      {
+        client_id: this.clientId,
+        grant_type: 'password',
+        scope: 'openid'
+      },
+      userData
+    );
 
-  this.oauth = new RestClient(options.baseUrl + '/oauth/:type', clientOptions);
-  this.oauthWithIDTokenValidation = new OAUthWithIDTokenValidation(this.oauth, options);
-  this.clientId = options.clientId;
-  this.clientSecret = options.clientSecret;
-};
+    if (!userData || typeof userData !== 'object') {
+      throw new ArgumentError('Missing user data object');
+    }
 
-/**
- * Sign in using a username and password.
- *
- * @method    signIn
- * @memberOf  module:auth.OAuthAuthenticator.prototype
- *
- * @example <caption>
- *   Given the user's credentials and the connection specified, it
- *   will return a JSON with the access_token and id_token.
- *   More information in the
- *   <a href="https://auth0.com/docs/api/authentication#database-ad-ldap-active-">
- *     API Docs
- *   </a>.
- * </caption>
- *
- * var data = {
- *   client_id: '{CLIENT_ID}',  // Optional field.
- *   username: '{USERNAME}',
- *   password: '{PASSWORD}',
- *   connection: '{CONNECTION_NAME}',
- *   scope: 'openid'  // Optional field.
- * };
- *
- * auth0.oauth.signIn(data, function (err, userData) {
- *   if (err) {
- *     // Handle error.
- *   }
- *
- *   console.log(userData);
- * });
- *
- * @param   {Object}    userData              User credentials object.
- * @param   {String}    userData.username     Username.
- * @param   {String}    userData.password     User password.
- * @param   {String}    userData.connection   The identity provider in use.
- *
- * @return  {Promise|undefined}
- */
-OAuthAuthenticator.prototype.signIn = function(userData, cb) {
-  var params = {
-    type: 'ro'
-  };
-  var defaultFields = {
-    client_id: this.clientId,
-    grant_type: 'password',
-    scope: 'openid'
-  };
-  var data = extend(defaultFields, userData);
+    if (typeof data.connection !== 'string' || data.connection.split().length === 0) {
+      throw new ArgumentError('connection field is required');
+    }
 
-  if (!userData || typeof userData !== 'object') {
-    throw new ArgumentError('Missing user data object');
+    if (cb && cb instanceof Function) {
+      return this.oauthWithIDTokenValidation.create(params, data, cb);
+    }
+
+    return this.oauthWithIDTokenValidation.create(params, data);
   }
 
-  if (typeof data.connection !== 'string' || data.connection.split().length === 0) {
-    throw new ArgumentError('connection field is required');
+  /**
+   * Sign in using a username and password
+   *
+   * @method    passwordGrant
+   * @memberOf  module:auth.OAuthAuthenticator.prototype
+   *
+   * @example <caption>
+   *   Given the user's credentials perform the OAuth password grant
+   *   or Password Realm grant if a realm is provided,
+   *   it will return a JSON with the access_token and id_token.
+   *   More information in the
+   *   <a href="https://auth0.com/docs/api/authentication#resource-owner-password">
+   *     API Docs
+   *   </a>.
+   * </caption>
+   *
+   * const data = {
+   *   client_id: '{CLIENT_ID}',  // Optional field.
+   *   username: '{USERNAME}',
+   *   password: '{PASSWORD}',
+   *   realm: '{CONNECTION_NAME}', // Optional field.
+   *   scope: 'openid'  // Optional field.
+   * };
+   *
+   * auth0.oauth.passwordGrant(data, function (err, userData) {
+   *   if (err) {
+   *     // Handle error.
+   *   }
+   *
+   *   console.log(userData);
+   * });
+   *
+   * @param   {Object}    userData              User credentials object.
+   * @param   {String}    userData.username     Username.
+   * @param   {String}    userData.password     User password.
+   * @param   {String}    [userData.realm]      Name of the realm to use to authenticate or the connection name
+   *
+   * @return  {Promise|undefined}
+   */
+  passwordGrant(userData, cb) {
+    const params = { type: 'token' };
+    const data = Object.assign(
+      {
+        client_id: this.clientId,
+        client_secret: this.clientSecret,
+        grant_type: 'password'
+      },
+      userData
+    );
+
+    if (!userData || typeof userData !== 'object') {
+      throw new ArgumentError('Missing user data object');
+    }
+
+    if (typeof data.username !== 'string' || data.username.split().length === 0) {
+      throw new ArgumentError('username field is required');
+    }
+
+    if (typeof data.password !== 'string' || data.password.split().length === 0) {
+      throw new ArgumentError('password field is required');
+    }
+
+    if (typeof data.realm === 'string' && data.realm.split().length !== 0) {
+      data.grant_type = 'http://auth0.com/oauth/grant-type/password-realm';
+    }
+
+    if (cb && cb instanceof Function) {
+      return this.oauthWithIDTokenValidation.create(params, data, cb);
+    }
+
+    return this.oauthWithIDTokenValidation.create(params, data);
   }
 
-  if (cb && cb instanceof Function) {
-    return this.oauthWithIDTokenValidation.create(params, data, cb);
+  /**
+   * Sign in using a social provider access token.
+   *
+   * @method    socialSignIn
+   * @memberOf  module:auth.OAuthAuthenticator.prototype
+   *
+   * @param   {Object}    data                User credentials object.
+   * @param   {String}    data.access_token   User access token.
+   * @param   {String}    data.connection     Identity provider.
+   *
+   * @return  {Promise|undefined}
+   */
+  socialSignIn(data, cb) {
+    const params = { type: 'access_token' };
+
+    if (typeof data !== 'object') {
+      throw new ArgumentError('Missing user credential objects');
+    }
+
+    if (typeof data.access_token !== 'string' || data.access_token.trim().length === 0) {
+      throw new ArgumentError('access_token field is required');
+    }
+
+    if (typeof data.connection !== 'string' || data.connection.trim().length === 0) {
+      throw new ArgumentError('connection field is required');
+    }
+
+    if (cb && cb instanceof Function) {
+      return this.oauth.create(params, data, cb);
+    }
+
+    return this.oauth.create(params, data);
   }
 
-  return this.oauthWithIDTokenValidation.create(params, data);
-};
+  clientCredentialsGrant(options, cb) {
+    const params = { type: 'token' };
+    const { clientId: client_id, clientSecret: client_secret } = this;
 
-/**
- * Sign in using a username and password
- *
- * @method    passwordGrant
- * @memberOf  module:auth.OAuthAuthenticator.prototype
- *
- * @example <caption>
- *   Given the user's credentials perform the OAuth password grant
- *   or Password Realm grant if a realm is provided,
- *   it will return a JSON with the access_token and id_token.
- *   More information in the
- *   <a href="https://auth0.com/docs/api/authentication#resource-owner-password">
- *     API Docs
- *   </a>.
- * </caption>
- *
- * var data = {
- *   client_id: '{CLIENT_ID}',  // Optional field.
- *   username: '{USERNAME}',
- *   password: '{PASSWORD}',
- *   realm: '{CONNECTION_NAME}', // Optional field.
- *   scope: 'openid'  // Optional field.
- * };
- *
- * auth0.oauth.passwordGrant(data, function (err, userData) {
- *   if (err) {
- *     // Handle error.
- *   }
- *
- *   console.log(userData);
- * });
- *
- * @param   {Object}    userData              User credentials object.
- * @param   {String}    userData.username     Username.
- * @param   {String}    userData.password     User password.
- * @param   {String}    [userData.realm]      Name of the realm to use to authenticate or the connection name
- *
- * @return  {Promise|undefined}
- */
-OAuthAuthenticator.prototype.passwordGrant = function(userData, cb) {
-  var params = {
-    type: 'token'
-  };
-  var defaultFields = {
-    client_id: this.clientId,
-    client_secret: this.clientSecret,
-    grant_type: 'password'
-  };
-  var data = extend(defaultFields, userData);
+    const data = Object.assign(
+      {
+        grant_type: 'client_credentials',
+        client_id,
+        client_secret
+      },
+      options
+    );
 
-  if (!userData || typeof userData !== 'object') {
-    throw new ArgumentError('Missing user data object');
+    if (!options || typeof options !== 'object') {
+      throw new ArgumentError('Missing options object');
+    }
+
+    if (!data.client_id || data.client_id.trim().length === 0) {
+      throw new ArgumentError('client_id field is required');
+    }
+
+    if (!data.client_secret || data.client_secret.trim().length === 0) {
+      throw new ArgumentError('client_secret field is required');
+    }
+
+    if (cb && cb instanceof Function) {
+      return this.oauth.create(params, data, cb);
+    }
+
+    return this.oauth.create(params, data);
   }
 
-  if (typeof data.username !== 'string' || data.username.split().length === 0) {
-    throw new ArgumentError('username field is required');
+  /**
+   * Sign in using an authorization code
+   *
+   * @method    authorizationCodeGrant
+   * @memberOf  module:auth.OAuthAuthenticator.prototype
+   *
+   * @example <caption>
+   *   Given the code returned in the URL params after the redirect
+   *   from successful authentication, exchange the code for auth0
+   *   credentials. It will return JSON with the access_token and id_token.
+   *   More information in the
+   *   <a href="https://auth0.com/docs/api/authentication#authorization-code-grant">
+   *     API Docs
+   *   </a>.
+   * </caption>
+   *
+   * const data = {
+   *   code: '{CODE}',
+   *   redirect_uri: '{REDIRECT_URI}',
+   *   client_id: '{CLIENT_ID}',  // Optional field.
+   *   client_secret: '{CLIENT_SECRET}',  // Optional field.
+   * };
+   *
+   * auth0.oauth.authorizationCodeGrant(data, function (err, userData) {
+   *   if (err) {
+   *     // Handle error.
+   *   }
+   *
+   *   console.log(userData);
+   * });
+   *
+   * @param   {Object}    data                  Authorization code payload
+   * @param   {String}    userData.code         Code in URL returned after authentication
+   * @param   {String}    userData.redirect_uri The URL to which Auth0 will redirect the browser after authorization has been granted by the user.
+   *
+   * @return  {Promise|undefined}
+   */
+  authorizationCodeGrant(options, cb) {
+    const params = { type: 'token' };
+    const { clientId: client_id, clientSecret: client_secret } = this;
+
+    const data = Object.assign(
+      {
+        grant_type: 'authorization_code',
+        client_id,
+        client_secret
+      },
+      options
+    );
+
+    if (!options || typeof options !== 'object') {
+      throw new ArgumentError('Missing options object');
+    }
+
+    if (!data.code || data.code.trim().length === 0) {
+      throw new ArgumentError('code field is required');
+    }
+
+    if (!data.redirect_uri || data.redirect_uri.trim().length === 0) {
+      throw new ArgumentError('redirect_uri field is required');
+    }
+
+    if (cb && cb instanceof Function) {
+      return this.oauthWithIDTokenValidation.create(params, data, cb);
+    }
+
+    return this.oauthWithIDTokenValidation.create(params, data);
   }
-
-  if (typeof data.password !== 'string' || data.password.split().length === 0) {
-    throw new ArgumentError('password field is required');
-  }
-
-  if (typeof data.realm === 'string' && data.realm.split().length !== 0) {
-    data.grant_type = 'http://auth0.com/oauth/grant-type/password-realm';
-  }
-
-  if (cb && cb instanceof Function) {
-    return this.oauthWithIDTokenValidation.create(params, data, cb);
-  }
-
-  return this.oauthWithIDTokenValidation.create(params, data);
-};
-
-/**
- * Sign in using a social provider access token.
- *
- * @method    socialSignIn
- * @memberOf  module:auth.OAuthAuthenticator.prototype
- *
- * @param   {Object}    data                User credentials object.
- * @param   {String}    data.access_token   User access token.
- * @param   {String}    data.connection     Identity provider.
- *
- * @return  {Promise|undefined}
- */
-OAuthAuthenticator.prototype.socialSignIn = function(data, cb) {
-  var params = {
-    type: 'access_token'
-  };
-
-  if (typeof data !== 'object') {
-    throw new ArgumentError('Missing user credential objects');
-  }
-
-  if (typeof data.access_token !== 'string' || data.access_token.trim().length === 0) {
-    throw new ArgumentError('access_token field is required');
-  }
-
-  if (typeof data.connection !== 'string' || data.connection.trim().length === 0) {
-    throw new ArgumentError('connection field is required');
-  }
-
-  if (cb && cb instanceof Function) {
-    return this.oauth.create(params, data, cb);
-  }
-
-  return this.oauth.create(params, data);
-};
-
-OAuthAuthenticator.prototype.clientCredentialsGrant = function(options, cb) {
-  var params = {
-    type: 'token'
-  };
-
-  var defaultFields = {
-    grant_type: 'client_credentials',
-    client_id: this.clientId,
-    client_secret: this.clientSecret
-  };
-
-  var data = extend(defaultFields, options);
-
-  if (!options || typeof options !== 'object') {
-    throw new ArgumentError('Missing options object');
-  }
-
-  if (!data.client_id || data.client_id.trim().length === 0) {
-    throw new ArgumentError('client_id field is required');
-  }
-
-  if (!data.client_secret || data.client_secret.trim().length === 0) {
-    throw new ArgumentError('client_secret field is required');
-  }
-
-  if (cb && cb instanceof Function) {
-    return this.oauth.create(params, data, cb);
-  }
-
-  return this.oauth.create(params, data);
-};
-
-/**
- * Sign in using an authorization code
- *
- * @method    authorizationCodeGrant
- * @memberOf  module:auth.OAuthAuthenticator.prototype
- *
- * @example <caption>
- *   Given the code returned in the URL params after the redirect
- *   from successful authentication, exchange the code for auth0
- *   credentials. It will return JSON with the access_token and id_token.
- *   More information in the
- *   <a href="https://auth0.com/docs/api/authentication#authorization-code-grant">
- *     API Docs
- *   </a>.
- * </caption>
- *
- * var data = {
- *   code: '{CODE}',
- *   redirect_uri: '{REDIRECT_URI}',
- *   client_id: '{CLIENT_ID}',  // Optional field.
- *   client_secret: '{CLIENT_SECRET}',  // Optional field.
- * };
- *
- * auth0.oauth.authorizationCodeGrant(data, function (err, userData) {
- *   if (err) {
- *     // Handle error.
- *   }
- *
- *   console.log(userData);
- * });
- *
- * @param   {Object}    data                  Authorization code payload
- * @param   {String}    userData.code         Code in URL returned after authentication
- * @param   {String}    userData.redirect_uri The URL to which Auth0 will redirect the browser after authorization has been granted by the user.
- *
- * @return  {Promise|undefined}
- */
-OAuthAuthenticator.prototype.authorizationCodeGrant = function(options, cb) {
-  var params = {
-    type: 'token'
-  };
-
-  var defaultFields = {
-    grant_type: 'authorization_code',
-    client_id: this.clientId,
-    client_secret: this.clientSecret
-  };
-
-  var data = extend(defaultFields, options);
-
-  if (!options || typeof options !== 'object') {
-    throw new ArgumentError('Missing options object');
-  }
-
-  if (!data.code || data.code.trim().length === 0) {
-    throw new ArgumentError('code field is required');
-  }
-
-  if (!data.redirect_uri || data.redirect_uri.trim().length === 0) {
-    throw new ArgumentError('redirect_uri field is required');
-  }
-
-  if (cb && cb instanceof Function) {
-    return this.oauthWithIDTokenValidation.create(params, data, cb);
-  }
-
-  return this.oauthWithIDTokenValidation.create(params, data);
-};
+}
 
 module.exports = OAuthAuthenticator;
