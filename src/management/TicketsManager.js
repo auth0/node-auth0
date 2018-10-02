1f0c22e410df4e0ae201d0b9daebfa3f7f4b9774
Vikas <potlurivikas@gmail.com>
2018-10-02 13:12:05 -0500

    chore(es6): Migrate src/management/TicketsManager.js

--- src/management/TicketsManager.js
+++ src/management/TicketsManager.js
@@ -1,6 +1,6 @@
-var ArgumentError = require('rest-facade').ArgumentError;
-var Auth0RestClient = require('../Auth0RestClient');
-var RetryRestClient = require('../RetryRestClient');
+const { ArgumentError } = require('rest-facade');
+const Auth0RestClient = require('../Auth0RestClient');
+const RetryRestClient = require('../RetryRestClient');
 
 /**
  * @class
@@ -13,103 +13,107 @@ var RetryRestClient = require('../RetryRestClient');
  * @param {Object} [options.headers]  Headers to be included in all requests.
  * @param {Object} [options.retry]    Retry Policy Config
  */
-var TicketsManager = function(options) {
-  if (options === null || typeof options !== 'object') {
-    throw new ArgumentError('Must provide manager options');
-  }
+class TicketsManager {
+  constructor(options) {
+    if (options === null || typeof options !== 'object') {
+      throw new ArgumentError('Must provide manager options');
+    }
 
-  if (options.baseUrl === null || options.baseUrl === undefined) {
-    throw new ArgumentError('Must provide a base URL for the API');
-  }
+    if (options.baseUrl === null || options.baseUrl === undefined) {
+      throw new ArgumentError('Must provide a base URL for the API');
+    }
 
-  if ('string' !== typeof options.baseUrl || options.baseUrl.length === 0) {
-    throw new ArgumentError('The provided base URL is invalid');
-  }
+    if ('string' !== typeof options.baseUrl || options.baseUrl.length === 0) {
+      throw new ArgumentError('The provided base URL is invalid');
+    }
 
-  var clientOptions = {
-    errorFormatter: { message: 'message', name: 'error' },
-    headers: options.headers,
-    query: { repeatParams: false }
-  };
+    const { headers, baseUrl, tokenProvider, retry } = options;
+
+    const clientOptions = {
+      errorFormatter: { message: 'message', name: 'error' },
+      headers,
+      query: { repeatParams: false }
+    };
+
+    /**
+     * Provides an abstraction layer for consuming the
+     * {@link https://auth0.com/docs/api/v2#!/Tickets Tickets endpoint}.
+     *
+     * @type {external:RestClient}
+     */
+    const auth0RestClient = new Auth0RestClient(
+      `${baseUrl}/tickets/:type`,
+      clientOptions,
+      tokenProvider
+    );
+    this.ticket = new RetryRestClient(auth0RestClient, retry);
+  }
 
   /**
-   * Provides an abstraction layer for consuming the
-   * {@link https://auth0.com/docs/api/v2#!/Tickets Tickets endpoint}.
+   * Create a new password change ticket.
    *
-   * @type {external:RestClient}
+   * @method    changePassword
+   * @memberOf  module:management.TicketsManager.prototype
+   *
+   * @example
+   * const params = {
+   *   result_url: '{REDIRECT_URL}',  // Redirect after using the ticket.
+   *   user_id: '{USER_ID}',  // Optional.
+   *   email: '{USER_EMAIL}',  // Optional.
+   *   new_password: '{PASSWORD}'
+   * };
+   *
+   * management.tickets.changePassword(params, function (err) {
+   *   if (err) {
+   *     // Handle error.
+   *   }
+   * });
+   *
+   * @param   {Function}  [cb]  Callback function.
+   * @return  {Promise}
    */
-  var auth0RestClient = new Auth0RestClient(
-    options.baseUrl + '/tickets/:type',
-    clientOptions,
-    options.tokenProvider
-  );
-  this.ticket = new RetryRestClient(auth0RestClient, options.retry);
-};
+  changePassword(data, cb) {
+    const params = { type: 'password-change' };
 
-/**
- * Create a new password change ticket.
- *
- * @method    changePassword
- * @memberOf  module:management.TicketsManager.prototype
- *
- * @example
- * var params = {
- *   result_url: '{REDIRECT_URL}',  // Redirect after using the ticket.
- *   user_id: '{USER_ID}',  // Optional.
- *   email: '{USER_EMAIL}',  // Optional.
- *   new_password: '{PASSWORD}'
- * };
- *
- * management.tickets.changePassword(params, function (err) {
- *   if (err) {
- *     // Handle error.
- *   }
- * });
- *
- * @param   {Function}  [cb]  Callback function.
- * @return  {Promise}
- */
-TicketsManager.prototype.changePassword = function(data, cb) {
-  var params = { type: 'password-change' };
+    if (cb && cb instanceof Function) {
+      return this.ticket.create(params, data, cb);
+    }
 
-  if (cb && cb instanceof Function) {
-    return this.ticket.create(params, data, cb);
+    // Return a promise.
+    return this.ticket.create(params, data);
   }
 
-  // Return a promise.
-  return this.ticket.create(params, data);
-};
+  /**
+   * Create an email verification ticket.
+   *
+   * @method    verifyEmail
+   * @memberOf  module:management.TicketsManager.prototype
+   *
+   * @example
+   * const data = {
+   *   user_id: '{USER_ID}',
+   *   result_url: '{REDIRECT_URL}' // Optional redirect after the ticket is used.
+   * };
+   *
+   * management.tickets.verifyEmail(data, function (err) {
+   *   if (err) {
+   *     // Handle error.
+   *   }
+   * });
+   *
+   * @param   {Function}  [cb]  Callback function.
+   * @return  {Promise}
+   */
+  verifyEmail(data, cb) {
+    const params = { type: 'email-verification' };
 
-/**
- * Create an email verification ticket.
- *
- * @method    verifyEmail
- * @memberOf  module:management.TicketsManager.prototype
- *
- * @example
- * var data = {
- *   user_id: '{USER_ID}',
- *   result_url: '{REDIRECT_URL}' // Optional redirect after the ticket is used.
- * };
- *
- * management.tickets.verifyEmail(data, function (err) {
- *   if (err) {
- *     // Handle error.
- *   }
- * });
- *
- * @param   {Function}  [cb]  Callback function.
- * @return  {Promise}
- */
-TicketsManager.prototype.verifyEmail = function(data, cb) {
-  var params = { type: 'email-verification' };
+    if (cb && cb instanceof Function) {
+      return this.ticket.create(params, data, cb);
+    }
 
-  if (cb && cb instanceof Function) {
-    return this.ticket.create(params, data, cb);
+    // Return a promise.
+    return this.ticket.create(params, data);
   }
-
-  // Return a promise.
-  return this.ticket.create(params, data);
-};
+}
 
 module.exports = TicketsManager;
