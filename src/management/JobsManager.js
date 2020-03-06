057063f1534551f4d192bf77c14336eb8b39ebfa
orangain <orangain@gmail.com>
2020-03-07 00:13:16 +0900

    Mark upsert and send_completion_email are optional
    Also update description.

--- src/management/JobsManager.js
+++ src/management/JobsManager.js
@@ -144,13 +144,13 @@ JobsManager.prototype.get = function(params, cb) {
  *   }
  * });
  *
- * @param   {Object}    data                        Users import data.
- * @param   {String}    data.connection_id          connection_id of the connection to which users will be imported.
- * @param   {String}    [data.users]                Path to the users data file. Either users or users_json is mandatory.
- * @param   {String}    [data.users_json]           JSON data for the users.
- * @param   {String}    data.upsert                 OPTIONAL: set to true to upsert users, defaults to false
- * @param   {String}    data.send_completion_email  OPTIONAL: defaults to true
- * @param   {Function}  [cb]                        Callback function.
+ * @param   {Object}    data                          Users import data.
+ * @param   {String}    data.connection_id            connection_id of the connection to which users will be imported.
+ * @param   {String}    [data.users]                  Path to the users data file. Either users or users_json is mandatory.
+ * @param   {String}    [data.users_json]             JSON data for the users.
+ * @param   {String}    [data.upsert]                 Whether to update users if they already exist (true) or to ignore them (false).
+ * @param   {String}    [data.send_completion_email]  Whether to send a completion email to all tenant owners when the job is finished (true) or not (false).
+ * @param   {Function}  [cb]                          Callback function.
  *
  * @return  {Promise|undefined}
  */
