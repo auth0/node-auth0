fc3611811e8a8209c23b9be84505071f4c025e59
alexesprit <alex.esprit@gmail.com>
2021-05-14 19:44:49 +0300

    Add method to verify OTP sent to email

--- test/auth/authentication-client.tests.js
+++ test/auth/authentication-client.tests.js
@@ -221,4 +221,40 @@ describe('AuthenticationClient', function() {
       this.client.verifySMSCode({ phone_number: '123', password: 'code' }, this.callback);
     });
   });
+
+  describe(`verifyEmailCode`, () => {
+    before(function() {
+      this.client = new AuthenticationClient({ token: 'token', domain: 'auth0.com' });
+      this.passwordlessMock = sinon.mock(this.client.passwordless);
+      this.callback = function() {};
+    });
+    it('should call signIn with otp if provided', function() {
+      this.passwordlessMock
+        .expects('signIn')
+        .once()
+        .withExactArgs(
+          {
+            username: '123',
+            realm: 'email',
+            otp: 'code'
+          },
+          this.callback
+        );
+      this.client.verifyEmailCode({ email: '123', otp: 'code' }, this.callback);
+    });
+    it('should call signIn with otp if provided', function() {
+      this.passwordlessMock
+        .expects('signIn')
+        .once()
+        .withExactArgs(
+          {
+            username: '123',
+            realm: 'email',
+            otp: 'code'
+          },
+          this.callback
+        );
+      this.client.verifyEmailCode({ username: '123', otp: 'code' }, this.callback);
+    });
+  });
 });
