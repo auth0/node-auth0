8699f6b8076574818706c5bd0135dea1a1e7b94f
frederikprijck <frederik.prijck@auth0.com>
2023-04-21 16:07:18 +0200

    feat: migrate rules tests

--- test/management/rules.tests.ts
+++ test/management/rules.tests.ts
@@ -1,18 +1,23 @@
-const { expect } = require('chai');
-const nock = require('nock');
+import chai from 'chai';
+import nock from 'nock';
 
-const API_URL = 'https://tenant.auth0.com';
+const API_URL = 'https://tenant.auth0.com/api/v2';
 
-const RulesManager = require(`../../src/management/RulesManager`);
-const { ArgumentError } = require('rest-facade');
+import { RulesManager } from '../../src/management/__generated/index';
+import { ManagementClient } from '../../src/management';
+
+const { expect } = chai;
 
 describe('RulesManager', () => {
+  let rules: RulesManager;
+  const token = 'TOKEN';
+
   before(function () {
-    this.token = 'TOKEN';
-    this.rules = new RulesManager({
-      headers: { authorization: `Bearer ${this.token}` },
-      baseUrl: API_URL,
+    const client = new ManagementClient({
+      domain: 'tenant.auth0.com',
+      token: token,
     });
+    rules = client.rules;
   });
 
   describe('instance', () => {
@@ -20,44 +25,34 @@ describe('RulesManager', () => {
 
     methods.forEach((method) => {
       it(`should have a ${method} method`, function () {
-        expect(this.rules[method]).to.exist.to.be.an.instanceOf(Function);
+        expect((rules as any)[method]).to.exist.to.be.an.instanceOf(Function);
       });
     });
   });
 
   describe('#constructor', () => {
-    it('should error when no options are provided', () => {
-      expect(() => {
-        new RulesManager();
-      }).to.throw(ArgumentError, 'Must provide manager options');
-    });
-
     it('should throw an error when no base URL is provided', () => {
       expect(() => {
-        new RulesManager({});
-      }).to.throw(ArgumentError, 'Must provide a base URL for the API');
+        new RulesManager({} as any);
+      }).to.throw(Error, 'Must provide a base URL for the API');
     });
 
     it('should throw an error when the base URL is invalid', () => {
       expect(() => {
         new RulesManager({ baseUrl: '' });
-      }).to.throw(ArgumentError, 'The provided base URL is invalid');
+      }).to.throw(Error, 'The provided base URL is invalid');
     });
   });
 
   describe('#getAll', () => {
-    beforeEach(function () {
-      this.request = nock(API_URL).get('/rules').reply(200);
-    });
+    let request: nock.Scope;
 
-    it('should accept a callback', function (done) {
-      this.rules.getAll(() => {
-        done();
-      });
+    beforeEach(function () {
+      request = nock(API_URL).get('/rules').reply(200, []);
     });
 
     it('should return a promise if no callback is given', function (done) {
-      this.rules.getAll().then(done.bind(null, null)).catch(done.bind(null, null));
+      rules.getAll().then(done.bind(null, null)).catch(done.bind(null, null));
     });
 
     it('should pass any errors to the promise catch handler', function (done) {
@@ -65,7 +60,7 @@ describe('RulesManager', () => {
 
       nock(API_URL).get('/rules').reply(500);
 
-      this.rules.getAll().catch((err) => {
+      rules.getAll().catch((err) => {
         expect(err).to.exist;
         done();
       });
@@ -74,21 +69,19 @@ describe('RulesManager', () => {
     it('should pass the body of the response to the "then" handler', async function () {
       nock.cleanAll();
 
-      const data = [{ test: true }];
+      const data = [{ id: '123' }];
       nock(API_URL).get('/rules').reply(200, data);
 
-      const credentials = await this.rules.getAll();
-      expect(credentials).to.be.an.instanceOf(Array);
+      const credentials = await rules.getAll();
+      expect(credentials.data).to.be.an.instanceOf(Array);
 
-      expect(credentials.length).to.equal(data.length);
+      expect(credentials.data.length).to.equal(data.length);
 
-      expect(credentials[0].test).to.equal(data[0].test);
+      expect(credentials.data[0].id).to.equal(data[0].id);
     });
 
     it('should perform a GET request to /api/v2/rules', async function () {
-      const { request } = this;
-
-      await this.rules.getAll();
+      await rules.getAll();
       expect(request.isDone()).to.be.true;
     });
 
@@ -97,10 +90,10 @@ describe('RulesManager', () => {
 
       const request = nock(API_URL)
         .get('/rules')
-        .matchHeader('Authorization', `Bearer ${this.token}`)
-        .reply(200);
+        .matchHeader('Authorization', `Bearer ${token}`)
+        .reply(200, []);
 
-      await this.rules.getAll();
+      await rules.getAll();
       expect(request.isDone()).to.be.true;
     });
 
@@ -111,49 +104,41 @@ describe('RulesManager', () => {
         include_fields: true,
         fields: 'test',
       };
-      const request = nock(API_URL).get('/rules').query(params).reply(200);
+      const request = nock(API_URL).get('/rules').query(params).reply(200, []);
 
-      await this.rules.getAll(params);
+      await rules.getAll(params);
       expect(request.isDone()).to.be.true;
     });
   });
 
   describe('#get', () => {
+    const data = {
+      id: '5',
+      name: 'Test rule',
+      enabled: true,
+      script: "function (user, contest, callback) { console.log('Test'); }",
+      stage: 'login_success',
+    };
+    let request: nock.Scope;
     beforeEach(function () {
-      this.data = {
-        id: 5,
-        name: 'Test rule',
-        enabled: true,
-        script: "function (user, contest, callback) { console.log('Test'); }",
-        stage: 'login_success',
-      };
-
-      this.request = nock(API_URL).get(`/rules/${this.data.id}`).reply(200, this.data);
-    });
-
-    it('should accept a callback', function (done) {
-      const params = { id: this.data.id };
-
-      this.rules.get(params, done.bind(null, null));
+      request = nock(API_URL).get(`/rules/${data.id}`).reply(200, data);
     });
 
     it('should return a promise if no callback is given', function (done) {
-      this.rules.get({ id: this.data.id }).then(done.bind(null, null)).catch(done.bind(null, null));
+      rules.get({ id: data.id }).then(done.bind(null, null)).catch(done.bind(null, null));
     });
 
     it('should perform a POST request to /api/v2/rules/5', async function () {
-      const { request } = this;
-
-      await this.rules.get({ id: this.data.id });
+      await rules.get({ id: data.id });
       expect(request.isDone()).to.be.true;
     });
 
     it('should pass any errors to the promise catch handler', function (done) {
       nock.cleanAll();
 
-      nock(API_URL).get(`/rules/${this.data.id}`).reply(500);
+      nock(API_URL).get(`/rules/${data.id}`).reply(500);
 
-      this.rules.get({ id: this.data.id }).catch((err) => {
+      rules.get({ id: data.id }).catch((err) => {
         expect(err).to.exist;
 
         done();
@@ -164,11 +149,11 @@ describe('RulesManager', () => {
       nock.cleanAll();
 
       const request = nock(API_URL)
-        .get(`/rules/${this.data.id}`)
-        .matchHeader('Authorization', `Bearer ${this.token}`)
-        .reply(200);
+        .get(`/rules/${data.id}`)
+        .matchHeader('Authorization', `Bearer ${token}`)
+        .reply(200, {});
 
-      await this.rules.get({ id: this.data.id });
+      await rules.get({ id: data.id });
       expect(request.isDone()).to.be.true;
     });
   });
@@ -181,19 +166,14 @@ describe('RulesManager', () => {
       script: "function (user, contest, callback) { console.log('Test'); }",
       stage: 'login_success',
     };
+    let request: nock.Scope;
 
     beforeEach(function () {
-      this.request = nock(API_URL).post('/rules').reply(200);
-    });
-
-    it('should accept a callback', function (done) {
-      this.rules.create(data, () => {
-        done();
-      });
+      request = nock(API_URL).post('/rules').reply(200, {});
     });
 
     it('should return a promise if no callback is given', function (done) {
-      this.rules.create(data).then(done.bind(null, null)).catch(done.bind(null, null));
+      rules.create(data).then(done.bind(null, null)).catch(done.bind(null, null));
     });
 
     it('should pass any errors to the promise catch handler', function (done) {
@@ -201,7 +181,7 @@ describe('RulesManager', () => {
 
       nock(API_URL).post('/rules').reply(500);
 
-      this.rules.create(data).catch((err) => {
+      rules.create(data).catch((err) => {
         expect(err).to.exist;
 
         done();
@@ -209,18 +189,16 @@ describe('RulesManager', () => {
     });
 
     it('should perform a POST request to /api/v2/rules', async function () {
-      const { request } = this;
-
-      await this.rules.create(data);
+      await rules.create(data);
       expect(request.isDone()).to.be.true;
     });
 
     it('should pass the data in the body of the request', async function () {
       nock.cleanAll();
 
-      const request = nock(API_URL).post('/rules', data).reply(200);
+      const request = nock(API_URL).post('/rules', data).reply(200, {});
 
-      await this.rules.create(data);
+      await rules.create(data);
       expect(request.isDone()).to.be.true;
     });
 
@@ -229,51 +207,46 @@ describe('RulesManager', () => {
 
       const request = nock(API_URL)
         .post('/rules')
-        .matchHeader('Authorization', `Bearer ${this.token}`)
-        .reply(200);
+        .matchHeader('Authorization', `Bearer ${token}`)
+        .reply(200, {});
 
-      await this.rules.create(data);
+      await rules.create(data);
       expect(request.isDone()).to.be.true;
     });
   });
 
   describe('#update', () => {
+    const data = { id: '5' };
+    let request: nock.Scope;
+
     beforeEach(function () {
-      this.data = { id: 5 };
-
-      this.request = nock(API_URL).patch(`/rules/${this.data.id}`).reply(200, this.data);
-    });
-
-    it('should accept a callback', function (done) {
-      this.rules.update({ id: 5 }, {}, done.bind(null, null));
+      request = nock(API_URL).patch(`/rules/${data.id}`).reply(200, data);
     });
 
     it('should return a promise if no callback is given', function (done) {
-      this.rules.update({ id: 5 }, {}).then(done.bind(null, null)).catch(done.bind(null, null));
+      rules.update({ id: '5' }, {}).then(done.bind(null, null)).catch(done.bind(null, null));
     });
 
     it('should perform a PATCH request to /api/v2/rules/5', async function () {
-      const { request } = this;
-
-      await this.rules.update({ id: 5 }, {});
+      await rules.update({ id: '5' }, {});
       expect(request.isDone()).to.be.true;
     });
 
     it('should include the new data in the body of the request', async function () {
       nock.cleanAll();
 
-      const request = nock(API_URL).patch(`/rules/${this.data.id}`, this.data).reply(200);
+      const request = nock(API_URL).patch(`/rules/${data.id}`, { name: 'new name' }).reply(200, {});
 
-      await this.rules.update({ id: 5 }, this.data);
+      await rules.update(data, { name: 'new name' });
       expect(request.isDone()).to.be.true;
     });
 
     it('should pass any errors to the promise catch handler', function (done) {
       nock.cleanAll();
 
-      nock(API_URL).patch(`/rules/${this.data.id}`).reply(500);
+      nock(API_URL).patch(`/rules/${data.id}`).reply(500);
 
-      this.rules.update({ id: this.data.id }, this.data).catch((err) => {
+      rules.update({ id: data.id }, { name: 'new name' }).catch((err) => {
         expect(err).to.exist;
 
         done();
@@ -282,24 +255,19 @@ describe('RulesManager', () => {
   });
 
   describe('#delete', () => {
-    const id = 5;
+    const id = '5';
+    let request: nock.Scope;
 
     beforeEach(function () {
-      this.request = nock(API_URL).delete(`/rules/${id}`).reply(200);
-    });
-
-    it('should accept a callback', function (done) {
-      this.rules.delete({ id }, done.bind(null, null));
+      request = nock(API_URL).delete(`/rules/${id}`).reply(200);
     });
 
     it('should return a promise when no callback is given', function () {
-      expect(this.rules.delete({ id })).instanceOf(Promise);
+      expect(rules.delete({ id })).instanceOf(Promise);
     });
 
     it(`should perform a delete request to /rules/${id}`, async function () {
-      const { request } = this;
-
-      await this.rules.delete({ id });
+      await rules.delete({ id });
       expect(request.isDone()).to.be.true;
     });
 
@@ -308,7 +276,7 @@ describe('RulesManager', () => {
 
       nock(API_URL).delete(`/rules/${id}`).reply(500);
 
-      this.rules.delete({ id }).catch((err) => {
+      rules.delete({ id }).catch((err) => {
         expect(err).to.exist;
 
         done();
@@ -320,10 +288,10 @@ describe('RulesManager', () => {
 
       const request = nock(API_URL)
         .delete(`/rules/${id}`)
-        .matchHeader('authorization', `Bearer ${this.token}`)
+        .matchHeader('authorization', `Bearer ${token}`)
         .reply(200);
 
-      await this.rules.delete({ id });
+      await rules.delete({ id });
       expect(request.isDone()).to.be.true;
     });
   });
