a3fb06975b32fc36d682da0a76d6227f82aac6e3
frederikprijck <frederik.prijck@auth0.com>
2023-04-25 13:23:58 +0200

    feat: migrate resource servers tests

--- test/management/resource-servers.tests.ts
+++ test/management/resource-servers.tests.ts
@@ -1,69 +1,51 @@
-const { expect } = require('chai');
-const nock = require('nock');
+import chai from 'chai';
+import nock from 'nock';
 
-const API_URL = 'https://tenant.auth0.com';
+const API_URL = 'https://tenant.auth0.com/api/v2';
 
-const ResourceServersManager = require(`../../src/management/ResourceServersManager`);
-const { ArgumentError } = require('rest-facade');
+import { ResourceServersManager } from '../../src/management/__generated/index';
+import { ManagementClient } from '../../src/management';
+
+const { expect } = chai;
 
 describe('ResourceServersManager', () => {
+  let resourceServers: ResourceServersManager;
+  const token = 'TOKEN';
+
   before(function () {
-    this.token = 'TOKEN';
-    this.resourceServers = new ResourceServersManager({
-      headers: {
-        authorization: `Bearer ${this.token}`,
-      },
-      baseUrl: API_URL,
+    const client = new ManagementClient({
+      domain: 'tenant.auth0.com',
+      token: token,
     });
+    resourceServers = client.resourceServers;
   });
 
   afterEach(() => {
     nock.cleanAll();
   });
 
-  describe('instance', () => {
-    const methods = ['get', 'create', 'update', 'delete'];
-
-    methods.forEach((method) => {
-      it(`should have a ${method} method`, function () {
-        expect(this.resourceServers[method]).to.exist.to.be.an.instanceOf(Function);
-      });
-    });
-  });
-
   describe('#constructor', () => {
-    it('should error when no options are provided', () => {
-      expect(() => {
-        new ResourceServersManager();
-      }).to.throw(ArgumentError, 'Must provide manager options');
-    });
-
     it('should throw an error when no base URL is provided', () => {
       expect(() => {
-        new ResourceServersManager({});
-      }).to.throw(ArgumentError, 'Must provide a base URL for the API');
+        new ResourceServersManager({} as any);
+      }).to.throw(Error, 'Must provide a base URL for the API');
     });
 
     it('should throw an error when the base URL is invalid', () => {
       expect(() => {
         new ResourceServersManager({ baseUrl: '' });
-      }).to.throw(ArgumentError, 'The provided base URL is invalid');
+      }).to.throw(Error, 'The provided base URL is invalid');
     });
   });
 
   describe('#getAll', () => {
+    let request: nock.Scope;
     beforeEach(function () {
-      this.request = nock(API_URL).get('/resource-servers').reply(200);
-    });
-
-    it('should accept a callback', function (done) {
-      this.resourceServers.getAll(() => {
-        done();
-      });
+      request = nock(API_URL).get('/resource-servers').reply(200, []);
     });
 
     it('should return a promise if no callback is given', function (done) {
-      this.resourceServers.getAll().then(done.bind(null, null)).catch(done.bind(null, null));
+      resourceServers.getAll().then(done.bind(null, null)).catch(done.bind(null, null));
     });
 
     it('should pass any errors to the promise catch handler', function (done) {
@@ -71,7 +53,7 @@ describe('ResourceServersManager', () => {
 
       nock(API_URL).get('/resource-servers').reply(500);
 
-      this.resourceServers.getAll().catch((err) => {
+      resourceServers.getAll().catch((err) => {
         expect(err).to.exist;
 
         done();
@@ -81,24 +63,22 @@ describe('ResourceServersManager', () => {
     it('should pass the body of the response to the "then" handler', function (done) {
       nock.cleanAll();
 
-      const data = [{ test: true }];
+      const data = [{ id: '123' }];
       nock(API_URL).get('/resource-servers').reply(200, data);
 
-      this.resourceServers.getAll().then((resourceServers) => {
-        expect(resourceServers).to.be.an.instanceOf(Array);
+      resourceServers.getAll().then((resourceServers) => {
+        expect(resourceServers.data).to.be.an.instanceOf(Array);
 
-        expect(resourceServers.length).to.equal(data.length);
+        expect(resourceServers.data.length).to.equal(data.length);
 
-        expect(resourceServers[0].test).to.equal(data[0].test);
+        expect(resourceServers.data[0].id).to.equal(data[0].id);
 
         done();
       });
     });
 
     it('should perform a GET request to /api/v2/resource-servers', function (done) {
-      const { request } = this;
-
-      this.resourceServers.getAll().then(() => {
+      resourceServers.getAll().then(() => {
         expect(request.isDone()).to.be.true;
 
         done();
@@ -110,10 +90,10 @@ describe('ResourceServersManager', () => {
 
       const request = nock(API_URL)
         .get('/resource-servers')
-        .matchHeader('Authorization', `Bearer ${this.token}`)
-        .reply(200);
+        .matchHeader('Authorization', `Bearer ${token}`)
+        .reply(200, []);
 
-      this.resourceServers.getAll().then(() => {
+      resourceServers.getAll().then(() => {
         expect(request.isDone()).to.be.true;
         done();
       });
@@ -121,24 +101,19 @@ describe('ResourceServersManager', () => {
   });
 
   describe('#get', () => {
-    const params = { id: 5 };
+    const params = { id: '5' };
     const data = {
       id: params.id,
       name: 'Test Resource Server',
     };
+    let request: nock.Scope;
 
     beforeEach(function () {
-      this.request = nock(API_URL).get(`/resource-servers/${data.id}`).reply(200);
-    });
-
-    it('should accept a callback', function (done) {
-      this.resourceServers.get(params, () => {
-        done();
-      });
+      request = nock(API_URL).get(`/resource-servers/${data.id}`).reply(200, {});
     });
 
     it('should return a promise if no callback is given', function (done) {
-      this.resourceServers.get(params).then(done.bind(null, null)).catch(done.bind(null, null));
+      resourceServers.get(params).then(done.bind(null, null)).catch(done.bind(null, null));
     });
 
     it('should pass any errors to the promise catch handler', function (done) {
@@ -146,7 +121,7 @@ describe('ResourceServersManager', () => {
 
       nock(API_URL).get(`/resource-servers/${params.id}`).reply(500);
 
-      this.resourceServers.get().catch((err) => {
+      resourceServers.get(params).catch((err) => {
         expect(err).to.exist;
 
         done();
@@ -158,17 +133,15 @@ describe('ResourceServersManager', () => {
 
       nock(API_URL).get(`/resource-servers/${params.id}`).reply(200, data);
 
-      this.resourceServers.get(params).then((connection) => {
-        expect(connection.id).to.equal(data.id);
+      resourceServers.get(params).then((resourceServer) => {
+        expect(resourceServer.data.id).to.equal(data.id);
 
         done();
       });
     });
 
     it('should perform a GET request to /api/v2/resource-servers/:id', function (done) {
-      const { request } = this;
-
-      this.resourceServers.get(params).then(() => {
+      resourceServers.get(params).then(() => {
         expect(request.isDone()).to.be.true;
 
         done();
@@ -179,11 +152,11 @@ describe('ResourceServersManager', () => {
       nock.cleanAll();
 
       const request = nock(API_URL)
-        .get('/resource-servers')
-        .matchHeader('Authorization', `Bearer ${this.token}`)
-        .reply(200);
+        .get(`/resource-servers/${params.id}`)
+        .matchHeader('Authorization', `Bearer ${token}`)
+        .reply(200, {});
 
-      this.resourceServers.get().then(() => {
+      resourceServers.get(params).then(() => {
         expect(request.isDone()).to.be.true;
 
         done();
@@ -195,20 +168,16 @@ describe('ResourceServersManager', () => {
     const data = {
       name: 'Acme Backend API',
       options: {},
+      identifier: '123',
     };
+    let request: nock.Scope;
 
     beforeEach(function () {
-      this.request = nock(API_URL).post('/resource-servers').reply(200, data);
-    });
-
-    it('should accept a callback', function (done) {
-      this.resourceServers.create(data, () => {
-        done();
-      });
+      request = nock(API_URL).post('/resource-servers').reply(200, data);
     });
 
     it('should return a promise if no callback is given', function (done) {
-      this.resourceServers.create(data).then(done.bind(null, null)).catch(done.bind(null, null));
+      resourceServers.create(data).then(done.bind(null, null)).catch(done.bind(null, null));
     });
 
     it('should pass any errors to the promise catch handler', function (done) {
@@ -216,7 +185,7 @@ describe('ResourceServersManager', () => {
 
       nock(API_URL).post('/resource-servers').reply(500);
 
-      this.resourceServers.create(data).catch((err) => {
+      resourceServers.create(data).catch((err) => {
         expect(err).to.exist;
 
         done();
@@ -224,9 +193,7 @@ describe('ResourceServersManager', () => {
     });
 
     it('should perform a POST request to /api/v2/resource-servers', function (done) {
-      const { request } = this;
-
-      this.resourceServers.create(data).then(() => {
+      resourceServers.create(data).then(() => {
         expect(request.isDone()).to.be.true;
 
         done();
@@ -236,9 +203,9 @@ describe('ResourceServersManager', () => {
     it('should pass the data in the body of the request', function (done) {
       nock.cleanAll();
 
-      const request = nock(API_URL).post('/resource-servers', data).reply(200);
+      const request = nock(API_URL).post('/resource-servers', data).reply(200, {});
 
-      this.resourceServers.create(data).then(() => {
+      resourceServers.create(data).then(() => {
         expect(request.isDone()).to.be.true;
 
         done();
@@ -250,10 +217,10 @@ describe('ResourceServersManager', () => {
 
       const request = nock(API_URL)
         .post('/resource-servers')
-        .matchHeader('Authorization', `Bearer ${this.token}`)
-        .reply(200);
+        .matchHeader('Authorization', `Bearer ${token}`)
+        .reply(200, {});
 
-      this.resourceServers.create(data).then(() => {
+      resourceServers.create(data).then(() => {
         expect(request.isDone()).to.be.true;
 
         done();
@@ -262,28 +229,20 @@ describe('ResourceServersManager', () => {
   });
 
   describe('#update', () => {
-    const params = { id: 5 };
+    const params = { id: '5' };
     const data = {
-      id: 5,
+      id: '5',
       name: 'Acme Backend API',
       options: {},
     };
+    let request: nock.Scope;
 
     beforeEach(function () {
-      this.request = nock(API_URL).patch(`/resource-servers/${data.id}`).reply(200, data);
-    });
-
-    it('should accept a callback', function (done) {
-      this.resourceServers.update(params, data, () => {
-        done();
-      });
+      request = nock(API_URL).patch(`/resource-servers/${data.id}`).reply(200, data);
     });
 
     it('should return a promise if no callback is given', function (done) {
-      this.resourceServers
-        .update(params, data)
-        .then(done.bind(null, null))
-        .catch(done.bind(null, null));
+      resourceServers.update(params, data).then(done.bind(null, null)).catch(done.bind(null, null));
     });
 
     it('should pass any errors to the promise catch handler', function (done) {
@@ -291,7 +250,7 @@ describe('ResourceServersManager', () => {
 
       nock(API_URL).patch(`/resource-servers/${data.id}`).reply(500);
 
-      this.resourceServers.update(params, data).catch((err) => {
+      resourceServers.update(params, data).catch((err) => {
         expect(err).to.exist.to.be.an.instanceOf(Error);
 
         done();
@@ -299,9 +258,7 @@ describe('ResourceServersManager', () => {
     });
 
     it('should perform a PATCH request to /api/v2/resource-servers/:id', function (done) {
-      const { request } = this;
-
-      this.resourceServers.update(params, data).then(() => {
+      resourceServers.update(params, data).then(() => {
         expect(request.isDone()).to.be.true;
 
         done();
@@ -311,9 +268,9 @@ describe('ResourceServersManager', () => {
     it('should pass the data in the body of the request', function (done) {
       nock.cleanAll();
 
-      const request = nock(API_URL).patch(`/resource-servers/${data.id}`, data).reply(200);
+      const request = nock(API_URL).patch(`/resource-servers/${data.id}`, data).reply(200, {});
 
-      this.resourceServers.update(params, data).then(() => {
+      resourceServers.update(params, data).then(() => {
         expect(request.isDone()).to.be.true;
 
         done();
@@ -325,10 +282,10 @@ describe('ResourceServersManager', () => {
 
       const request = nock(API_URL)
         .patch(`/resource-servers/${data.id}`)
-        .matchHeader('Authorization', `Bearer ${this.token}`)
-        .reply(200);
+        .matchHeader('Authorization', `Bearer ${token}`)
+        .reply(200, {});
 
-      this.resourceServers.update(params, data).then(() => {
+      resourceServers.update(params, data).then(() => {
         expect(request.isDone()).to.be.true;
 
         done();
@@ -337,24 +294,23 @@ describe('ResourceServersManager', () => {
   });
 
   describe('#delete', () => {
-    const id = 5;
+    const id = '5';
+    let request: nock.Scope;
 
     beforeEach(function () {
-      this.request = nock(API_URL).delete(`/resource-servers/${id}`).reply(200);
+      request = nock(API_URL).delete(`/resource-servers/${id}`).reply(200);
     });
 
     it('should accept a callback', function (done) {
-      this.resourceServers.delete({ id }, done.bind(null, null));
+      resourceServers.delete({ id }, done.bind(null, null));
     });
 
     it('should return a promise when no callback is given', function (done) {
-      this.resourceServers.delete({ id }).then(done.bind(null, null));
+      resourceServers.delete({ id }).then(done.bind(null, null));
     });
 
     it(`should perform a DELETE request to /resource-servers/${id}`, function (done) {
-      const { request } = this;
-
-      this.resourceServers.delete({ id }).then(() => {
+      resourceServers.delete({ id }).then(() => {
         expect(request.isDone()).to.be.true;
 
         done();
@@ -366,7 +322,7 @@ describe('ResourceServersManager', () => {
 
       nock(API_URL).delete(`/resource-servers/${id}`).reply(500);
 
-      this.resourceServers.delete({ id }).catch((err) => {
+      resourceServers.delete({ id }).catch((err) => {
         expect(err).to.exist;
 
         done();
@@ -378,10 +334,10 @@ describe('ResourceServersManager', () => {
 
       const request = nock(API_URL)
         .delete(`/resource-servers/${id}`)
-        .matchHeader('Authorization', `Bearer ${this.token}`)
-        .reply(200);
+        .matchHeader('Authorization', `Bearer ${token}`)
+        .reply(200, {});
 
-      this.resourceServers.delete({ id }).then(() => {
+      resourceServers.delete({ id }).then(() => {
         expect(request.isDone()).to.be.true;
 
         done();
