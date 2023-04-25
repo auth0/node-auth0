d5577e3905dea8cc120f6b0acde2902cbbc4bc74
adamjmcgrath <adam.mcgrath@okta.com>
2023-04-25 12:41:25 +0100

    Add code flow

--- src/auth/OAuth.ts
+++ src/auth/OAuth.ts
@@ -45,6 +45,24 @@ interface ClientCredentials {
   client_assertion_type?: 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer';
 }
 
+export interface AuthorizationCodeGrantRequest extends ClientCredentials {
+  /**
+   * The Authorization Code received from the initial `/authorize` call.
+   */
+  code: string;
+  /**
+   *	This is required only if it was set at the `/authorize` endpoint. The values must match.
+   */
+  redirect_uri?: string;
+}
+
+export interface AuthorizationCodeGrantWithPKCERequest extends AuthorizationCodeGrantRequest {
+  /**
+   * Cryptographically random key that was used to generate the code_challenge passed to `/authorize`.
+   */
+  code_verifier: string;
+}
+
 export interface ClientCredentialsGrantRequest extends ClientCredentials {
   /**
    * The unique identifier of the target API you want to access.
@@ -56,6 +74,95 @@ export interface ClientCredentialsGrantRequest extends ClientCredentials {
  *  OAuth 2.0 flows.
  */
 export default class OAuth extends BaseAuthAPI {
+  /**
+   * This is the flow that regular web apps use to access an API.
+   *
+   * Use this endpoint to exchange an Authorization Code for a Token.
+   *
+   * See: https://auth0.com/docs/api/authentication#authorization-code-flow44
+   *
+   * @example
+   * ```js
+   * const auth0 = new AuthenitcationApi({
+   *    domain: 'my-domain.auth0.com',
+   *    clientId: 'myClientId',
+   *    clientSecret: 'myClientSecret'
+   * });
+   *
+   * await auth0.oauth.authorizationCodeGrant({ code: 'mycode' });
+   * ```
+   */
+  async authorizationCodeGrant(
+    bodyParameters: AuthorizationCodeGrantRequest,
+    initOverrides?: InitOverride
+  ): Promise<JSONApiResponse<TokenSet>> {
+    validateRequiredRequestParams(bodyParameters, ['code']);
+    const response = await this.request(
+      {
+        path: '/oauth/token',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams(
+          (await this.addClientAuthentication(
+            { ...bodyParameters, grant_type: 'authorization_code' },
+            true
+          )) as Record<string, string>
+        ),
+      },
+      initOverrides
+    );
+
+    return JSONApiResponse.fromResponse(response);
+  }
+
+  /**
+   * PKCE was originally designed to protect the authorization code flow in mobile apps,
+   * but its ability to prevent authorization code injection makes it useful for every type of OAuth client,
+   * even web apps that use client authentication.
+   *
+   * See: https://auth0.com/docs/api/authentication#authorization-code-flow-with-pkce45
+   *
+   * @example
+   * ```js
+   * const auth0 = new AuthenitcationApi({
+   *    domain: 'my-domain.auth0.com',
+   *    clientId: 'myClientId',
+   *    clientSecret: 'myClientSecret'
+   * });
+   *
+   * await auth0.oauth.authorizationCodeGrantWithPKCE({
+   *   code: 'mycode',
+   *   code_verifier: 'mycodeverifier'
+   * });
+   * ```
+   */
+  async authorizationCodeGrantWithPKCE(
+    bodyParameters: AuthorizationCodeGrantWithPKCERequest,
+    initOverrides?: InitOverride
+  ): Promise<JSONApiResponse<TokenSet>> {
+    validateRequiredRequestParams(bodyParameters, ['code', 'code_verifier']);
+    const response = await this.request(
+      {
+        path: '/oauth/token',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams(
+          (await this.addClientAuthentication(
+            { ...bodyParameters, grant_type: 'authorization_code' },
+            false
+          )) as Record<string, string>
+        ),
+      },
+      initOverrides
+    );
+
+    return JSONApiResponse.fromResponse(response);
+  }
+
   /**
    * This is the OAuth 2.0 grant that server processes use to access an API.
    *
