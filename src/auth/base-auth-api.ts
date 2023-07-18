97ae84135f7cd3fa89c496c25875d1351a5d4d30
frederikprijck <frederik.prijck@auth0.com>
2023-07-18 08:56:25 +0200

    Handle duplicate user sign up

--- src/auth/base-auth-api.ts
+++ src/auth/base-auth-api.ts
@@ -33,6 +33,22 @@ export class AuthApiError extends Error {
   }
 }
 
+function parseErrorBody(body: any): AuthApiErrorResponse {
+  const rawData = JSON.parse(body);
+  let data: AuthApiErrorResponse;
+
+  if (rawData.code) {
+    data = {
+      error: rawData.code,
+      error_description: rawData.description,
+    };
+  } else {
+    data = rawData as AuthApiErrorResponse;
+  }
+
+  return data;
+}
+
 async function parseError(response: Response) {
   // Errors typically have a specific format:
   // {
@@ -41,10 +57,10 @@ async function parseError(response: Response) {
   // }
 
   const body = await response.text();
-  let data: AuthApiErrorResponse;
 
   try {
-    data = JSON.parse(body) as AuthApiErrorResponse;
+    const data = parseErrorBody(body);
+
     return new AuthApiError(
       data.error,
       data.error_description,
--- test/auth/database.test.ts
+++ test/auth/database.test.ts
@@ -1,10 +1,12 @@
 import nock from 'nock';
 import { beforeAll, afterAll } from '@jest/globals';
 import { Database } from '../../src/auth/database';
+import { AuthApiError } from '../../src/auth/base-auth-api';
 
 const { back: nockBack } = nock;
 
 const EMAIL = 'test-email@example.com';
+const DUPLICATE_EMAIL = 'test-email-duplicate@example.com';
 const PASSWORD = 'test-password';
 
 const opts = {
@@ -47,6 +49,30 @@ describe('Database', () => {
           password: PASSWORD,
         } as any)
       ).rejects.toThrow('Required parameter requestParameters.connection was null or undefined.');
+    });
+
+    it('should handle duplicate user error', async () => {
+      const database = new Database(opts);
+      const email = DUPLICATE_EMAIL;
+      let error: AuthApiError | null = null;
+
+      try {
+        await database.signUp({
+          email,
+          password: PASSWORD,
+          connection: 'Username-Password-Authentication',
+        });
+      } catch (e) {
+        error = e as AuthApiError;
+      }
+
+      expect(error).toBeDefined();
+      expect(error).toEqual(
+        expect.objectContaining({
+          error: 'invalid_signup',
+          error_description: 'Invalid sign up',
+        })
+      );
     });
   });
 
