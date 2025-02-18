493ceb629869c4a300aed5bcb3d5508ac2ba8812
tanya732 <sinha.tanya26@gmail.com>
2025-02-18 15:52:27 +0530

    Added support for BYOK for node-auth0

--- test/management/keys-manager.test.ts
+++ test/management/keys-manager.test.ts
@@ -1,55 +1,147 @@
-import nock from 'nock';
-import { KeysManager, ManagementClient } from '../../src/index.js';
-import { afterAll, beforeAll } from '@jest/globals';
+import {
+  KeysManager,
+  ManagementClient,
+  DeleteEncryptionKeyRequest,
+  PostEncryptionRequest,
+  GetSigningKeyRequest,
+  PutSigningKeysRequest,
+  GetEncryptionKeyRequest,
+  PostEncryptionRequestTypeEnum,
+  PostEncryptionKeyOperationRequest,
+  PostEncryptionKeyRequest,
+  PostEncryptionWrappingKeyRequest,
+} from '../../src/index.js';
 
-const { back: nockBack } = nock;
+import { checkMethod } from './tests.util.js';
 
-describe('AnomalyManager', () => {
-  let keysManager: KeysManager;
+const DOMAIN = `tenant.auth0.com`;
+const token = 'TOKEN';
 
-  let nockDone: () => void;
+describe('KeysManager', () => {
+  const flowsManager: KeysManager = new ManagementClient({ domain: DOMAIN, token }).keys;
 
-  beforeAll(async () => {
-    ({ nockDone } = await nockBack('management/fixtures/keys.json'));
+  // this is the test for the method flowsManager.getAll
+  describe('getAll', () => {
+    const operation = flowsManager.getAll();
+    const uri = `/keys/signing`;
+    const method = 'get';
 
-    const client = new ManagementClient({
-      domain: 'test-domain.auth0.com',
-      token: 'TOKEN',
-    });
-    keysManager = client.keys;
+    checkMethod({ operation, uri, method });
   });
 
-  afterAll(() => {
-    nockDone();
+  // this is the test for the method flowsManager.get
+  describe('get', () => {
+    const requestParameters: GetSigningKeyRequest = {
+      kid: 'kid',
+    };
+    const operation = flowsManager.get(requestParameters);
+    const uri = `/keys/signing/kid`;
+    const method = 'get';
+
+    checkMethod({ operation, uri, method });
   });
 
-  it('should get a list of keys', async () => {
-    await expect(keysManager.getAll()).resolves.toMatchObject({
-      data: expect.arrayContaining([expect.objectContaining({ kid: 'my-kid' })]),
-    });
+  // this is the test for the method flowsManager.rotate
+  describe('rotate', () => {
+    const operation = flowsManager.rotate();
+    const uri = `/keys/signing/rotate`;
+    const method = 'post';
+
+    checkMethod({ operation, uri, method });
   });
 
-  it('should get a key', async () => {
-    await expect(keysManager.get({ kid: 'my-kid' })).resolves.toMatchObject({
-      data: expect.objectContaining({ kid: 'my-kid' }),
-    });
+  // this is the test for the method flowsManager.revole
+  describe('revoke', () => {
+    const requestBody: PutSigningKeysRequest = {
+      kid: 'kid',
+    };
+    const operation = flowsManager.revoke(requestBody);
+    const uri = `/keys/signing/kid/revoke`;
+    const method = 'put';
+
+    checkMethod({ operation, uri, method });
   });
 
-  it('should rotate the tenant signing key', async () => {
-    await expect(keysManager.rotate()).resolves.toMatchObject({
-      status: 200,
-    });
+  // this is the test for the method flowsManager.revoke
+  describe('postEncryptionRekey', () => {
+    const operation = flowsManager.postEncryptionRekey();
+    const uri = `/keys/encryption/rekey`;
+    const method = 'post';
+
+    checkMethod({ operation, uri, method });
   });
 
-  it('should revoke a key', async () => {
-    await expect(keysManager.revoke({ kid: 'my-kid' })).resolves.toMatchObject({
-      status: 200,
-    });
+  // this is the test for the method flowsManager.deleteEncryptionKey
+  describe('deleteEncryptionKey', () => {
+    const requestParameters: DeleteEncryptionKeyRequest = {
+      kid: 'kid',
+    };
+    const operation = flowsManager.deleteEncryptionKey(requestParameters);
+    const uri = `/keys/encryption/kid`;
+    const method = 'delete';
+
+    checkMethod({ operation, uri, method });
   });
 
-  it('should rekey the tenant key heirarchy', async () => {
-    await expect(keysManager.postEncryptionRekey()).resolves.toMatchObject({
-      status: 200,
-    });
+  // this is the test for the method flowsManager.getEncryptionKey
+  describe('getEncryptionKey', () => {
+    const requestParameters: GetEncryptionKeyRequest = {
+      kid: 'kid',
+    };
+    const operation = flowsManager.getEncryptionKey(requestParameters);
+    const uri = `/keys/encryption/kid`;
+    const method = 'get';
+
+    checkMethod({ operation, uri, method });
+  });
+
+  // this is the test for the method flowsManager.getAllEncryptionKeys
+  describe('getAllEncryptionKeys', () => {
+    const operation = flowsManager.getAllEncryptionKeys();
+    const uri = `/keys/encryption`;
+    const method = 'get';
+
+    checkMethod({ operation, uri, method });
+  });
+
+  // this is the test for the method flowsManager.createEncryptionKey
+  describe('createEncryptionKey', () => {
+    const requestBody: PostEncryptionRequest = {
+      type: 'customer-provided-root-key' as PostEncryptionRequestTypeEnum,
+    };
+
+    const operation = flowsManager.createEncryptionKey(requestBody);
+    const uri = `/keys/encryption`;
+    const method = 'post';
+
+    checkMethod({ operation, uri, method });
+  });
+
+  // this is the test for the method flowsManager.importEncryptionKey
+  describe('importEncryptionKey', () => {
+    const requestBody: PostEncryptionKeyRequest = {
+      wrapped_key: 'wrapped-key',
+    };
+
+    const requestParameters: PostEncryptionKeyOperationRequest = {
+      kid: 'kid',
+    };
+    const operation = flowsManager.importEncryptionKey(requestParameters, requestBody);
+    const uri = `/keys/encryption/kid`;
+    const method = 'post';
+
+    checkMethod({ operation, uri, method });
+  });
+
+  // this is the test for the method flowsManager.createPublicWrappingKey
+  describe('createPublicWrappingKey', () => {
+    const requestParameters: PostEncryptionWrappingKeyRequest = {
+      kid: 'kid',
+    };
+    const operation = flowsManager.createPublicWrappingKey(requestParameters);
+    const uri = `/keys/encryption/kid/wrapping-key`;
+    const method = 'post';
+
+    checkMethod({ operation, uri, method });
   });
 });
