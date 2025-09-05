d9163c28686fc03c8114863c4b3e27338b83ab46
Kunal Dawar <kunal.dawar@okta.com>
2025-09-05 17:04:24 +0530

    fix(docs): streamline tar creation and extraction process in workflow

--- scripts/fix-versions.js
+++ scripts/fix-versions.js
@@ -25,9 +25,9 @@ const VERSIONS_JS_FILE = path.join(DOCS_DIR, 'versions.js');
  * Get current version from package.json
  */
 function getCurrentVersion() {
-  const packageJsonPath = path.join(__dirname, '../package.json');
-  const packageJson = JSON.parse(readFileSync(packageJsonPath, 'utf8'));
-  return packageJson.version;
+    const packageJsonPath = path.join(__dirname, '../package.json');
+    const packageJson = JSON.parse(readFileSync(packageJsonPath, 'utf8'));
+    return packageJson.version;
 }
 
 /**
