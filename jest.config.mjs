34776753b685574b6c01d4e791a9a769dfc31c33
Kunal Dawar <kunal.dawar@okta.com>
2025-10-17 12:32:56 +0530

    Fix: core.Supplier Type in ManagementClient & Remove Browser Tests

--- jest.config.mjs
+++ jest.config.mjs
@@ -35,17 +35,6 @@ export default {
             setupFilesAfterEnv: [],
         },
         {
-            displayName: "browser",
-            preset: "ts-jest",
-            testEnvironment: "<rootDir>/src/management/tests/BrowserTestEnvironment.ts",
-            moduleNameMapper: {
-                "^(\\.{1,2}/.*)\\.js$": "$1",
-            },
-            roots: ["<rootDir>/src/management/tests"],
-            testMatch: ["<rootDir>/src/management/tests/unit/**/?(*.)+(browser).(spec|test).[jt]s?(x)"],
-            setupFilesAfterEnv: [],
-        },
-        {
             displayName: "wire",
             preset: "ts-jest",
             testEnvironment: "node",
